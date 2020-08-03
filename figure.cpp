8d9f1138a41f65122b4052fc92d5571fb8102e4e
IDDQDD <welcometoheeeell@gmail.com>
2020-08-03 09:25:20 +0300

    release

--- figure.cpp
+++ figure.cpp
@@ -1,10 +1,7 @@
 #include "figure.h"
-Figure::Figure(std::initializer_list<Point>&& points)
-{
-  coordinates = points;
-}
+Figure::Figure(std::initializer_list<Point>&& points): coordinates(points){};
 
-  const std::vector<Point>& Figure::Get_Coordinates()
+const std::vector<Point>& Figure::Get_Coordinates()
   {
     return coordinates;
   }
