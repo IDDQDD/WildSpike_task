7d973af43d678ab1f835bcbd23f9ccfeb883af3c
IDDQDD <39268229+IDDQDD@users.noreply.github.com>
2020-08-03 14:39:54 +0300

    Delete figure.cpp

--- figure.cpp
+++
@@ -1,7 +0,0 @@
-#include "figure.h"
-Figure::Figure(std::initializer_list<Point>&& points): coordinates(points){};
-
-const std::vector<Point>& Figure::Get_Coordinates()
-  {
-    return coordinates;
-  }
